0ddef3568f5d3add09ecbe381446282aec49ee97
joshooaj <7669972+joshooaj@users.noreply.github.com>
2025-01-21 09:19:11 -0800

    tests: Fix output module path after setting up nbgv for versioning

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -1,15 +1,22 @@
 properties {
+    & dotnet tool restore
     $PSBPreference.General.ModuleVersion = (dotnet nbgv get-version -f json | ConvertFrom-Json).SimpleVersion
     $PSBPreference.Build.CompileModule = $true
     $PSBPreference.Help.DefaultLocale = 'en-US'
     $PSBPreference.Test.OutputFile = 'out/testResults.xml'
 
     $psake.context.tasks.stagefiles.PostAction = {
+        Write-Verbose "Updating ModuleVersion in output manifest to NBGV-based version $($PSBPreference.General.ModuleVersion)"
         $outputManifestPath = [io.path]::Combine($PSBPreference.Build.ModuleOutDir, "$($PSBPreference.General.ModuleName).psd1")
         Update-Metadata -Path $outputManifestPath -PropertyName ModuleVersion -Value $PSBPreference.General.ModuleVersion
+
+        Write-Verbose "Converting root module to UTF8 since PowerShellBuild generates a Unicode file on Windows PowerShell"
+        Import-Module (Join-Path $env:BHProjectPath 'tests/MetaFixers.psm1')
+        $rootModulePath = Join-Path -Path $env:BHBuildOutput -ChildPath "$($env:BHProjectName).psm1"
+        Get-Item $rootModulePath | ConvertTo-UTF8
     }
 }
 
 task Default -depends Test
 
-task Test -FromModule PowerShellBuild -minimumVersion '0.6.1'
+task Test -FromModule PowerShellBuild -minimumVersion '0.6.2'
--- tests/Help.tests.ps1
+++ tests/Help.tests.ps1
@@ -16,7 +16,7 @@ BeforeDiscovery {
     $outputDir            = Join-Path -Path $env:BHProjectPath -ChildPath 'Output'
     $outputModDir         = Join-Path -Path $outputDir -ChildPath $env:BHProjectName
     $outputModVerDir      = Join-Path -Path $outputModDir -ChildPath $manifest.ModuleVersion
-    $outputModVerManifest = Join-Path -Path $outputModVerDir -ChildPath "$($env:BHProjectName).psd1"
+    $outputModVerManifest = Join-Path -Path $env:BHBuildOutput -ChildPath "$($env:BHProjectName).psd1"
 
     # Get module commands
     # Remove all versions of the module from the session. Pester can't handle multiple versions.
