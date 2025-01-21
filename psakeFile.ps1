f6442830ca22379329d531323f5996dc41dbc83e
joshooaj <7669972+joshooaj@users.noreply.github.com>
2025-01-21 08:25:43 -0800

    ci: Install nbgv for module versioning

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -1,8 +1,13 @@
 properties {
-    # Set this to $true to create a module with a monolithic PSM1
+    $PSBPreference.General.ModuleVersion = (dotnet nbgv get-version -f json | ConvertFrom-Json).SimpleVersion
     $PSBPreference.Build.CompileModule = $true
     $PSBPreference.Help.DefaultLocale = 'en-US'
     $PSBPreference.Test.OutputFile = 'out/testResults.xml'
+
+    $psake.context.tasks.stagefiles.PostAction = {
+        $outputManifestPath = [io.path]::Combine($PSBPreference.Build.ModuleOutDir, "$($PSBPreference.General.ModuleName).psd1")
+        Update-Metadata -Path $outputManifestPath -PropertyName ModuleVersion -Value $PSBPreference.General.ModuleVersion
+    }
 }
 
 task Default -depends Test
