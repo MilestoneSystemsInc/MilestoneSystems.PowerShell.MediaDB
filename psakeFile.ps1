681dff9cf027037cb4c6addfa1becefbf08fa3c4
joshooaj <7669972+joshooaj@users.noreply.github.com>
2025-01-22 16:51:01 -0800

    ci: ffs

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -6,22 +6,16 @@ properties {
     $PSBPreference.Test.OutputFile = 'out/testResults.xml'
 
     $psake.context.tasks.stagefiles.PostAction = {
-        Write-Verbose "Updating ModuleVersion in output manifest to NBGV-based version $($PSBPreference.General.ModuleVersion)"
         $outputManifestPath = [io.path]::Combine($PSBPreference.Build.ModuleOutDir, "$($PSBPreference.General.ModuleName).psd1")
-        #BuildHelpers\Update-Metadata -Path $outputManifestPath -PropertyName ModuleVersion -Value $PSBPreference.General.ModuleVersion
-        $content = Get-Content $outputManifestPath | ForEach-Object { $_ -replace "'0.1.0'", "'$($PSBPreference.General.ModuleVersion)'" }
-        $content | Set-Content $outputManifestPath -Encoding UTF8
+        Write-Verbose "Updating ModuleVersion in output manifest '$outputManifestPath' to NBGV-based version $($PSBPreference.General.ModuleVersion)"
+        BuildHelpers\Update-Metadata -Path $outputManifestPath -Property ModuleVersion -Value $PSBPreference.General.ModuleVersion
+        #$content = Get-Content $outputManifestPath | ForEach-Object { $_ -replace "'0.1.0'", "'$($PSBPreference.General.ModuleVersion)'" }
+        #$content | Set-Content $outputManifestPath -Encoding UTF8
 
         Write-Verbose "Converting root module to UTF8 since PowerShellBuild generates a Unicode file on Windows PowerShell"
         Import-Module (Join-Path $env:BHProjectPath 'tests/MetaFixers.psm1')
         $rootModulePath = Join-Path -Path $env:BHBuildOutput -ChildPath "$($env:BHProjectName).psm1"
         Get-Item $rootModulePath | ConvertTo-UTF8
-    }
-
-    $module = Import-Module PowerShellBuild -PassThru
-    & $module {
-        Write-Host (Get-Command Update-Metadata | Out-String)
-        Write-Host (Get-Help Update-Metadata -Full | Out-String)
     }
 }
 
