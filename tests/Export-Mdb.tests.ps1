75424e879348e1672c0a1d777ded4e6f99272b4e
joshooaj <7669972+joshooaj@users.noreply.github.com>
2025-03-04 13:48:47 -0800

    tests: adjust export tests for ci

--- tests/Export-Mdb.tests.ps1
+++ tests/Export-Mdb.tests.ps1
@@ -13,15 +13,13 @@ Context "Export-Mdb" {
         Open-MediaDatabase -Path $script:EncryptedDbPath -Password $password
 
         $folder = Get-MdbCamera | Get-MdbSequence -Type RecordingSequence | ForEach-Object {
-            $item = [videoos.platform.configuration]::Instance.GetItem($_.FQID)
             $path = 'TestDrive:\{0}_{1}_Database' -f $_.Source, $_.Start.ToString('yyyy-MM-dd_HH-mm-ss')
             $exportParams = @{
-                Device = $item
                 Start  = $_.Start
                 End    = $_.End
                 Path   = $path
             }
-            Export-Mdb @exportParams
+            $_ | Export-Mdb @exportParams
         }
         $folder | Should -Not -BeNullOrEmpty
         (Test-Path $folder -PathType Container) | Should -Be $true
