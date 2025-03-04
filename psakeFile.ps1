7b965cef208787f0b02c805a5c25bafa3db9a945
joshooaj <7669972+joshooaj@users.noreply.github.com>
2025-03-04 13:38:12 -0800

    ci: Overwrite Get-Metadata alias with global scope

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -1,4 +1,8 @@
 properties {
+    if (Get-Alias Get-Metadata -ErrorAction SilentlyContinue) {
+        Set-Alias -Name Get-Metadata -Value BuildHelpers\Get-Metadata -Scope Global
+    }
+
     & dotnet tool restore
     $PSBPreference.General.ModuleVersion = (dotnet nbgv get-version -f json | ConvertFrom-Json).SimpleVersion
     $PSBPreference.Build.CompileModule = $true
@@ -8,12 +12,7 @@ properties {
     $psake.context.tasks.stagefiles.PostAction = {
         $outputManifestPath = [io.path]::Combine($PSBPreference.Build.ModuleOutDir, "$($PSBPreference.General.ModuleName).psd1")
         Write-Verbose "Updating ModuleVersion in output manifest '$outputManifestPath' to NBGV-based version $($PSBPreference.General.ModuleVersion)"
-
-        if (Get-Alias Get-Metadata -ErrorAction SilentlyContinue) {
-            Set-Alias -Name Get-Metadata -Value BuildHelpers\Get-Metadata
-        }
         Update-Metadata -Path $outputManifestPath -Value $PSBPreference.General.ModuleVersion
-
         Write-Verbose "Converting root module to UTF8 since PowerShellBuild generates a Unicode file on Windows PowerShell"
         Import-Module (Join-Path $env:BHProjectPath 'tests/MetaFixers.psm1')
         $rootModulePath = Join-Path -Path $env:BHBuildOutput -ChildPath "$($env:BHProjectName).psm1"
